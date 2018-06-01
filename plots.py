65950cb05b051ad30e1d8815ff29f9eaf4c12e60
treilly94 <treilly94@live.com>
2018-06-01 14:17:27 +0100

    Got api scratch working

--- plots.py
+++ plots.py
@@ -41,8 +41,50 @@ def get_timeseries():
             title='time'
         )
     )
+    fig = go.Figure(data=data, layout=layout)
 
-    return pyoff.plot(dict(data=data, layout=layout),
+    return pyoff.plot(fig,
                       show_link=False,
                       output_type="div",
                       include_plotlyjs=False)
+
+
+def get_week():
+    # Read data
+    df = pd.read_csv("./scratch/test_data/timeseries_test_data.csv")
+    df.DateTime = pd.to_datetime(df.DateTime)
+
+    days = [
+        df[df.DateTime.dt.weekday == 0].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 1].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 2].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 3].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 4].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 5].groupby(df.DateTime.dt.hour).size(),
+        df[df.DateTime.dt.weekday == 6].groupby(df.DateTime.dt.hour).size()
+    ]
+
+    data = []
+    for d in days:
+        # Add to trace
+        trace = go.Scatter(
+            x=d.index,
+            y=d,
+            mode="lines"
+        )
+        data.append(trace)
+
+    # Create layout
+    layout = dict(
+        title='Scatter',
+        xaxis=dict()
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    print(days[0])
+
+    return pyoff.plot(fig,
+                      show_link=False,
+                      output_type="div",
+                      include_plotlyjs=False)
+
--- scratch/sheets_quickstart.py
+++ scratch/sheets_quickstart.py
@@ -3,16 +3,16 @@ Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
 based on this tutorial https://developers.google.com/sheets/api/quickstart/python#step_3_set_up_the_sample
 """
 from __future__ import print_function
-from googleapiclient.discovery import build
+from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-store = file.Storage('cred.json')
+store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('./scratch/client_secrets.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
