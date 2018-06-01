fcdb95aaa74e1fec2182d2e050adfb6984d94fa0
treilly94 <treilly94@live.com>
2018-06-01 16:49:33 +0100

    added potential new pages

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+
 from scripts import plots as plt
+from scripts.googleAPI import SheetsAPI
 
 app = Flask(__name__)
+
+sheets_api = SheetsAPI()
+
+# Sheet IDs
+car_sheet = ""
+music_sheet = ""
+work_sheet = ""
 
 
 @app.route("/")
@@ -14,6 +23,21 @@ def index():
 @app.route("/car")
 def car():
     return render_template("car.html")
+
+
+@app.route("/music")
+def music():
+    return render_template("music.html")
+
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+
+@app.route("/fitness")
+def fitness():
+    return render_template("fitness.html")
 
 
 if __name__ == '__main__':
